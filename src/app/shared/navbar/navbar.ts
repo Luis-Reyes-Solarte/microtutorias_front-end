c2faaa8121798f806da1bb1f8c8fb01ad63d83b3
Luis Carlos Reyes Solarte <reyesluiscarlos920@gmail.com>
2026-05-18 18:34:49 -0500

    style: Cambios en la estructura de las barras de navegación.

--- src/app/shared/navbar/navbar.ts
+++ src/app/shared/navbar/navbar.ts
@@ -1,19 +1,38 @@
-import { Component, inject, Output, EventEmitter } from '@angular/core';
+import { Component, inject, Output, EventEmitter, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, MatToolbarModule, MatIconModule, MatButtonModule],
+  imports: [RouterLink, MatToolbarModule, MatIconModule, MatButtonModule, MatMenuModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
   private readonly auth = inject(AuthService);
   readonly authService = this.auth;
+
+  roleLabel = signal('');
+
+  constructor() {
+    this.updateRoleLabel();
+  }
+
+  private updateRoleLabel() {
+    if (this.auth.isAdmin()) {
+      this.roleLabel.set('Administrador');
+    } else if (this.auth.isTutor() && this.auth.isStudent()) {
+      this.roleLabel.set('Tutor / Estudiante');
+    } else if (this.auth.isTutor()) {
+      this.roleLabel.set('Tutor');
+    } else if (this.auth.isStudent()) {
+      this.roleLabel.set('Estudiante');
+    }
+  }
 
   @Output() toggle = new EventEmitter<void>();
 }
